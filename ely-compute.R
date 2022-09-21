9b47ca3c4a1998684ae79f5503b15537b409a74c
wkmor1 <willi@mmorris.email>
2022-09-21 15:17:24 +0300

    Catch errors for compute process

--- ely-compute.R
+++ ely-compute.R
@@ -1,62 +1,93 @@
-geoms <-
-  pool::dbListObjects(con, DBI::Id(schema = "subsets")) |>
-  getElement("table") |>
-  lapply(methods::slot, "name") |>
-  vapply(getElement, "", "table")
+tryCatch(
 
-for (geom in geoms) {
+  {
 
-  tbl <- DBI::Id(schema = "subsets", table = geom)
+    all_subsets <-
+      dplyr::tbl(con, DBI::Id(schema = "subsets", table = "mod_time")) |>
+      dplyr::summarise(n = dplyr::n()) |>
+      dplyr::pull(n) |>
+      as.character() |>
+      identical(Sys.getenv("N_SUBSETS"))
 
-  ely <-
-    dplyr::tbl(con, tbl) |>
-    dplyr::group_by(
-      taxon = ifelse(is.na(taxon_id), reported_name, taxon_id),
-      dplyr::across(
-        dplyr::all_of(sub("wgs84", "euref", c(select[["grp"]], facts[["grp"]])))
-      )
-    ) |>
-    mutate(
-      dplyr::across(
-        dplyr::all_of(select[["sum"]]),
-        ~ {
-          if (all(is.na(.x))) NA else sum(.x, na.rm = TRUE)
+    if (all_subsets) {
+
+      geoms <-
+        pool::dbListObjects(con, DBI::Id(schema = "subsets")) |>
+        getElement("table") |>
+        lapply(methods::slot, "name") |>
+        vapply(getElement, "", "table")
+
+      for (geom in geoms) {
+
+        tbl <- DBI::Id(schema = "subsets", table = geom)
+
+        ely <-
+          dplyr::tbl(con, tbl) |>
+          dplyr::group_by(
+            taxon = ifelse(is.na(taxon_id), reported_name, taxon_id),
+            dplyr::across(
+              dplyr::all_of(
+                sub("wgs84", "euref", c(select[["grp"]], facts[["grp"]]))
+              )
+            )
+          ) |>
+          mutate(
+            dplyr::across(
+              dplyr::all_of(select[["sum"]]),
+              ~ {
+                if (all(is.na(.x))) NA else sum(.x, na.rm = TRUE)
+              }
+            ),
+            dplyr::across(
+              dplyr::all_of(select[["max"]]),
+              ~ {
+                if (all(is.na(.x))) NA else max(.x, na.rm = TRUE)
+              }
+            )
+          ) |>
+          dplyr::distinct() |>
+          dplyr::summarise(
+            dplyr::across(
+              dplyr::all_of(c(select[["sum"]], select[["max"]])),
+              ~ {
+                if (all(is.na(.x))) NA else max(.x, na.rm = TRUE)
+              }
+            ),
+            dplyr::across(
+              dplyr::all_of(c(select[["collapse"]], facts[["collapse"]])),
+              ~ {
+                if (all(is.na(.x))) NA else stringr::str_flatten(.x, "; ")
+              }
+            ),
+            .groups = "keep"
+          ) |>
+          dplyr::ungroup() |>
+          dplyr::select(!c(taxon, occurrence_status))
+
+        tbl <- DBI::Id(schema = "ely", table = geom)
+
+        if (pool::dbExistsTable(con, tbl)) {
+
+          pool::dbRemoveTable(con, tbl)
+
         }
-      ),
-      dplyr::across(
-        dplyr::all_of(select[["max"]]),
-        ~ {
-          if (all(is.na(.x))) NA else max(.x, na.rm = TRUE)
-        }
-      )
-    ) |>
-    dplyr::distinct() |>
-    dplyr::summarise(
-      dplyr::across(
-        dplyr::all_of(c(select[["sum"]], select[["max"]])),
-        ~ {
-          if (all(is.na(.x))) NA else max(.x, na.rm = TRUE)
-        }
-      ),
-      across(
-        all_of(c(select[["collapse"]], facts[["collapse"]])),
-        ~ {
-          if (all(is.na(.x))) NA else stringr::str_flatten(.x, collapse = "; ")
-        }
-      ),
-      .groups = "keep"
-    ) |>
-    dplyr::ungroup() |>
-    dplyr::select(!c(taxon, occurrence_status))
 
-  tbl <- DBI::Id(schema = "ely", table = geom)
+        ely <- dplyr::compute(ely, tbl, temporary = FALSE)
 
-  if (pool::dbExistsTable(con, tbl)) {
+      }
 
-    pool::dbRemoveTable(con, tbl)
+    }
+
+  },
+  error = function(e) {
+
+    message(sprintf("ERROR [%s] %s", Sys.time(), e[["message"]]))
+
+    "false"
 
   }
 
-  ely <- dplyr::compute(ely, tbl, temporary = FALSE)
+) |>
+cat(file = "compute-success.txt")
 
-}
+cat(format(Sys.time(), usetz = TRUE), file = "compute-last-update.txt")
