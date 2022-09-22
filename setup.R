79c243c530330cc6e6ae1dfbba7bfdb268cde3ad
wkmor1 <willi@mmorris.email>
2022-09-22 17:30:27 +0300

    Lint script

--- setup.R
+++ setup.R
@@ -4,7 +4,7 @@ library(finbif, quietly = TRUE)
 options(
   finbif_api_url = Sys.getenv("FINBIF_API_URL"),
   finbif_warehouse_query = Sys.getenv("FINBIF_WAREHOUSE"),
-  finbif_email =Sys.getenv("FINBIF_EMAIL")
+  finbif_email = Sys.getenv("FINBIF_EMAIL")
 )
 
 if (identical(getOption("finbif_api_url"), "https://apitest.laji.fi")) {
