2188f20a71489c2110cbef69980eb5649bf25309
wkmor1 <willi@mmorris.email>
2022-09-23 17:41:54 +0300

    Update process script

--- oc-process.sh
+++ oc-process.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+i="all"
+
+while getopts ":t:e:i::" flag; do
+  case $flag in
+    t) t=${OPTARG} ;;
+    e) e=${OPTARG} ;;
+    i) i=${OPTARG} ;;
+  esac
+done
+
 set -a
 
-source ./$2
+source ./$e
 
 set +a
 
@@ -10,12 +20,46 @@ BRANCH=$(git symbolic-ref --short -q HEAD)
 
 if [ "$BRANCH" != "main" ]; then
 
-FINBIF_ACCESS_TOKEN=$FINBIF_DEV_ACCESS_TOKEN
-FINBIF_API_URL=$FINBIF_DEV_API_URL
+  FINBIF_ACCESS_TOKEN=$FINBIF_DEV_ACCESS_TOKEN
+  FINBIF_API_URL=$FINBIF_DEV_API_URL
 
 fi
 
-oc process -f $1 \
+if [ $i = "build" ]; then
+
+  ITEM=".items[0]"
+
+elif [ $i = "image" ]; then
+
+  ITEM=".items[1]"
+
+elif [ $i = "deploy" ]; then
+
+  ITEM=".items[2]"
+
+elif [ $i = "volume" ]; then
+
+  ITEM=".items[3]"
+
+elif [ $i = "service" ]; then
+
+  ITEM=".items[4]"
+
+elif [ $i = "route" ]; then
+
+  ITEM=".items[5]"
+
+elif [ $i = "job" ]; then
+
+  ITEM=".items[6]"
+
+else
+
+  ITEM=""
+
+fi
+
+oc process -f $t \
   -p BRANCH=$BRANCH \
   -p DB_PORT=$DB_PORT \
   -p DB_NAME=$DB_NAME \
@@ -27,4 +71,5 @@ oc process -f $1 \
   -p FINBIF_API_URL=$FINBIF_API_URL \
   -p FINBIF_EMAIL=$FINBIF_EMAIL \
   -p FINBIF_WAREHOUSE=$FINBIF_WAREHOUSE \
-  -p N_SUBSETS=$N_SUBSETS
+  -p N_SUBSETS=$N_SUBSETS \
+  | jq $ITEM
