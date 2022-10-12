b368192d88940287ef269ce67797c0d6566c8948
wkmor1 <willi@mmorris.email>
2022-10-12 09:58:11 +0300

    Cast port env var to int

--- init.R
+++ init.R
@@ -6,4 +6,4 @@ p <- pr_static(p, "/", "./var")
 
 p <- pr_set_docs(p, FALSE)
 
-pr_run(p, host = "0.0.0.0", port = Sys.getenv("SVR_PORT"))
+pr_run(p, host = "0.0.0.0", port = as.integer(Sys.getenv("SVR_PORT")))
