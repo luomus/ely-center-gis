d93404ce8d34f3823bb89798a15f875c2670c03f
wkmor1 <willi@mmorris.email>
2022-10-12 14:56:26 +0300

    Log api requests

--- init.R
+++ init.R
@@ -1,8 +1,46 @@
-library(plumber)
+library(plumber, quietly = TRUE)
+library(logger, quietly = TRUE)
 
 if (!dir.exists("var/status")) dir.create("var/status")
 
+if (!dir.exists("var/logs")) dir.create("var/logs")
+
+log_appender(appender_tee(tempfile("plumber_", "var/logs", ".log")))
+
+convert_empty <- function(x) switch(paste0(".", x), . = "-", x)
+
 p <- pr()
+
+p[["registerHooks"]](
+  list(
+    preroute = function() tictoc::tic(),
+    postroute = function(req, res) {
+
+      end <- tictoc::toc(quiet = TRUE)
+
+      log_fn <- log_info
+
+      if (res[["status"]] >= 400L) log_fn <- log_error
+
+      if (identical(req[["PATH_INFO"]], "/healthz")) log_fn <- \(.) {}
+
+      if (identical(req[["HTTP_USER_AGENT"]], "Zabbix")) log_fn <- \(.) {}
+
+      log_fn(
+        paste0(
+          '{convert_empty(req$REMOTE_ADDR)} ',
+          '"{convert_empty(req$HTTP_USER_AGENT)}" ',
+          '{convert_empty(req$HTTP_HOST)} ',
+          '{convert_empty(req$REQUEST_METHOD)} ',
+          '{convert_empty(req$PATH_INFO)} ',
+          '{convert_empty(res$status)} ',
+          '{round(end$toc - end$tic, digits = getOption("digits", 5L))}'
+        )
+      )
+
+    }
+  )
+)
 
 p <- pr_filter(
   p,
@@ -13,9 +51,9 @@ p <- pr_filter(
       req[["argsQuery"]][["access_token"]], Sys.getenv("USER_ACCESS_TOKEN")
     )
 
-    status <- grepl("status", req[["PATH_INFO"]])
+    status <- grepl("status|healthz", req[["PATH_INFO"]])
 
-    if (token | status) {
+    if (token || status) {
 
       forward()
 
@@ -28,6 +66,8 @@ p <- pr_filter(
 
   }
 )
+
+p <- pr_get(p, "/health", \() "")
 
 p <- pr_static(p, "/", "./var")
 
