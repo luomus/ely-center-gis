394acf717442c5edc1bd34345fb103085ebda5f7
wkmor1 <willi@mmorris.email>
2023-07-25 13:59:44 +0300

    Run job script via API

--- api.R
+++ api.R
@@ -8,7 +8,11 @@ function(req, res) {
 
   status <- grepl("status|healthz", req[["PATH_INFO"]])
 
-  if (token || status) {
+  secret <- identical(req[["argsQuery"]][["secret"]], Sys.getenv("JOB_SECRET"))
+
+  job <- secret && grepl("job", req[["PATH_INFO"]])
+
+  if (token || status || job) {
 
     forward()
 
@@ -29,6 +33,35 @@ function(req, res) {
 #* @serializer unboxedJSON
 function() {
   ""
+}
+
+#* Run job
+#* @get /job
+#* @tag status
+#* @response 200 A json object
+#* @serializer unboxedJSON
+function() {
+
+  on.exit({
+
+    sink(type = "message")
+
+    sink()
+
+  })
+
+  log_file_name <- sprintf("/var/logs/job-%s.txt", Sys.Date())
+
+  log_file <- file(log_file_name, open = "wt")
+
+  sink(log_file)
+
+  sink(log_file, type = "message")
+
+  source("ely.R")
+
+  "success"
+
 }
 
 #* @assets ./var /
